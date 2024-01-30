13fddd61965ac30151cd30a856afaaf974dec741
Danat Karasaev <104631630+mnenadoeloo@users.noreply.github.com>
2024-01-31 00:46:59 +0300

    bert_model.py

---
+++ bert_model.py
@@ -0,0 +1,54 @@
+import evaluate
+from tqdm.auto import tqdm
+from transformers import get_scheduler, BertForSequenceClassification, BertConfig
+
+model = BertForSequenceClassification.from_pretrained(
+    "bert-base-uncased",
+    num_labels = 4
+)
+
+outputs = model(**batch)
+
+optimizer = AdamW(model.parameters(),
+                  lr = 2e-5,
+                  eps = 1e-8)
+
+num_epochs = 3
+num_training_steps = num_epochs * len(train_dataloader)
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model.to(device)
+
+progress_bar = tqdm(range(num_training_steps))
+
+model.train()
+for epoch in range(num_epochs):
+    for batch in train_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = model(**batch)
+        loss = outputs.loss
+        loss.backward()
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+metric = evaluate.load("accuracy")
+model.eval()
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = model(**batch)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+    metric.add_batch(predictions=predictions, references=batch["labels"])
+
+metric.compute()
